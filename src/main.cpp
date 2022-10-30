a0c3fff842286afacfd0a5f2d10703479824d416
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-10-30 21:32:30 +0100

    Working Speed and Volume

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <Arduino.h>
+#include "XT_I2S_Audio.h"
+#include "WavData.h"
+#include "MusicDefinitions.h"
 
-void setup() {
-  // put your setup code here, to run once:
+//    SD Card
+#define SD_CS 5 // SD Card chip select
+
+//    I2S
+#define I2S_DOUT 22 // i2S Data out oin
+#define I2S_BCLK 26 // Bit clock
+#define I2S_LRC 25  // Left/Right clock, also known as Frame clock or word select
+
+#define POT_SPEED_WAV1_ANALOG_IN 32 // Pin that will connect to the middle pin of the potentiometer.
+#define POT_VOL_WAV1_ANALOG_IN 34   // Pin that will connect to the middle pin of the potentiometer.
+
+
+XT_I2S_Class I2SAudio(I2S_LRC, I2S_BCLK, I2S_DOUT, I2S_NUM_0);
+
+XT_Wav_Class MySound(WavData16BitStereo);
+
+//------------------------------------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------------------------------------
+// Defines
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(9600); // Used for info/debug
+
+  MySound.RepeatForever = true;
+  MySound.Volume = 100;
+
+  I2SAudio.Play(&MySound);
+}
+
+void loop()
+{
+  I2SAudio.FillBuffer();
+  MySound.Speed = floatMap(analogRead(POT_SPEED_WAV1_ANALOG_IN), 0, 4095, 0.0, 4.0);
+
+  MySound.Volume = floatMap(analogRead(POT_VOL_WAV1_ANALOG_IN), 0, 4095, 0, 150);
 }
